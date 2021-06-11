32c5c814aedd0b79c85bc0ca6811a1d67c057c83
fyupanquia <fyupanquia@outlook.com>
2021-06-11 16:00:38 -0500

    authorizer

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -10,6 +10,8 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function createAuction(event, context) {
   //const { title } = JSON.parse(event.body);
   const { title } = event.body; //httpJsonBodyParser
+  const { email } = event.requestContext.authorizer;
+
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -23,6 +25,7 @@ async function createAuction(event, context) {
     highestBid: {
       amount: 0,
     },
+    seller: email,
   };
 
   try {
--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,7 +10,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
-
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
 
   if (auction.status !== "OPEN") {
@@ -23,12 +23,24 @@ async function placeBid(event, context) {
     );
   }
 
+  // Bid identity validation
+  if (email === auction.seller) {
+    throw new createError.Forbidden(`You cannoit bid on your own auctions!`);
+  }
+
+  // Avoid double bidding
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder`);
+  }
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: "set highestBid.amount = :amount",
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder= :bidder",
     ExpressionAttributeValues: {
       ":amount": amount,
+      ":bidder": email,
     },
     ReturnValues: "ALL_NEW",
   };
